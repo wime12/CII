b2db3fc68e286ca3cf8bf698b4b34a237a3218ab
Wilfried Meindl <wilfried.meindl@gmail.com>
2016-05-12 14:26:56 +0200

    Use random strings and random numbers in map example

--- examples/map.c
+++ examples/map.c
@@ -6,8 +6,25 @@
 #include "mem.h"
 #include "atom.h"
 
+#define NCHARS 10
+#define NELEMS 1000000
+
+void random_string(char *s, const int len) {
+    static const char alphanum[] =
+	"0123456789"
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"abcdefghijklmnopqrstuvwxyz";
+    static const int alphanum_len = sizeof(alphanum) - 1;
+
+    for (int i = 0; i < len; ++i) {
+	s[i] = alphanum[rand() % alphanum_len];
+    }
+
+    s[len] = 0;
+}
+
 int cmp(const char *key1, const char *key2, void *cl) {
-    printf("cmp(\"%s\", \"%s\") = %d\n", key1, key2, strcmp(key1, key2));
+    // printf("cmp(\"%s\", \"%s\") = %d\n", key1, key2, strcmp(key1, key2));
     return strcmp(key1, key2);
 }
 
@@ -19,46 +36,71 @@ int *copy_data(const int *data) {
 }
 
 void free_data(int *data) {
-    printf("Free data: %d\n", *data);
+    // printf("Free data: %d\n", *data);
     FREE(data);
 }
 
-int pr(const char *key, const int *n, void *cl) {
-    printf("map[%s] = %d\n", key, *n);
+int pr(const void *key, const void *n, void *cl) {
+    // printf("map[\"%s\"] = %d\n", (char *)key, *(int *)n);
     return 0;
 }
 
 int main(void) {
+    char *line = NULL;
+    size_t j = 0;
+
     Map_T map = Map_new(
 	(Map_cmp_fun_T)cmp,
 	NULL,
 	(Map_copy_fun_T)copy_data,
 	NULL, (Map_free_fun_T)free_data);
 
-    int *n[6];
     srand(time(NULL));
-    for (int i = 0; i < 6; i++) {
-	NEW(n[i]);
-	*n[i] = rand() % 20;
+
+    puts("### Fill map");
+    getline(&line, &j, stdin);
+    FREE(line); j = 0;
+
+    char buf[NCHARS];
+    int n;
+
+    for (int i = 0; i < NELEMS; i++) {
+	n = rand();
+	random_string(buf, NCHARS);
+	Map_insert(map, Atom_string(buf), copy_data(&n));
     }
 
-    Map_insert(map, Atom_string("zbc"), n[0]);
-    Map_insert(map, Atom_string("def"), n[1]);
-    Map_insert(map, Atom_string("lkjas"), n[2]);
-    Map_insert(map, Atom_string("98zdfg"), n[3]);
-    Map_insert(map, Atom_string("ei"), n[4]);
-    Map_insert(map, Atom_string("pouaer"), n[5]);
+    puts("### Traverse map");
+    getline(&line, &j, stdin);
+    FREE(line); j = 0;
 
-    Map_traverse(map, (Map_apply_fun_T)pr, NULL);
+    Map_traverse(map, pr, NULL);
+
+    puts("### Copy map -> map2");
+    getline(&line, &j, stdin);
+    FREE(line); j = 0;
 
     Map_T map2 = Map_copy(map);
 
+    puts("### Free map");
+    getline(&line, &j, stdin);
+    FREE(line); j = 0;
+
     Map_free(&map);
 
-    puts("### Print copy");
-    Map_traverse(map2, (Map_apply_fun_T)pr, NULL);
+    puts("### Traverse map2");
+    getline(&line, &j, stdin);
+    FREE(line); j = 0;
+
+    Map_traverse(map2, pr, NULL);
+
+    puts("### Free map2");
+    getline(&line, &j, stdin);
+    FREE(line); j = 0;
 
     Map_free(&map2);
+
+    // printf("map = %p, map2 = %p\n", (void *)map, (void *)map2);
 
     return 0;
 }
