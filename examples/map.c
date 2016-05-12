e11c2f834c825c3984746119864c47a48c20c108
Wilfried Meindl <wilfried.meindl@gmail.com>
2016-05-12 13:18:29 +0200

    Add atom to map example

--- examples/map.c
+++ examples/map.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <stdlib.h>
 #include "map.h"
 #include "mem.h"
+#include "atom.h"
 
 int cmp(const char *key1, const char *key2, void *cl) {
     printf("cmp(\"%s\", \"%s\") = %d\n", key1, key2, strcmp(key1, key2));
     return strcmp(key1, key2);
-}
-
-char *copy_key(const char *key) {
-    size_t s = strlen(key);
-    char *new_key = CALLOC(s + 1, sizeof(char));
-    strcpy(new_key, key);
-    return new_key;
 }
 
 int *copy_data(const int *data) {
@@ -22,11 +18,8 @@ int *copy_data(const int *data) {
     return new_data;
 }
 
-void free_key(char *key) {
-    FREE(key);
-}
-
 void free_data(int *data) {
+    printf("Free data: %d\n", *data);
     FREE(data);
 }
 
@@ -38,22 +31,23 @@ int pr(const char *key, const int *n, void *cl) {
 int main(void) {
     Map_T map = Map_new(
 	(Map_cmp_fun_T)cmp,
-	(Map_copy_fun_T)copy_key,
+	NULL,
 	(Map_copy_fun_T)copy_data,
 	NULL, (Map_free_fun_T)free_data);
 
     int *n[6];
+    srand(time(NULL));
     for (int i = 0; i < 6; i++) {
 	NEW(n[i]);
-	*n[i] = i;
+	*n[i] = rand() % 20;
     }
 
-    Map_insert(map, "zbc", n[0]);
-    Map_insert(map, "def", n[1]);
-    Map_insert(map, "lkjas", n[2]);
-    Map_insert(map, "98zdfg", n[3]);
-    Map_insert(map, "ei", n[4]);
-    Map_insert(map, "pouaer", n[5]);
+    Map_insert(map, Atom_string("zbc"), n[0]);
+    Map_insert(map, Atom_string("def"), n[1]);
+    Map_insert(map, Atom_string("lkjas"), n[2]);
+    Map_insert(map, Atom_string("98zdfg"), n[3]);
+    Map_insert(map, Atom_string("ei"), n[4]);
+    Map_insert(map, Atom_string("pouaer"), n[5]);
 
     Map_traverse(map, (Map_apply_fun_T)pr, NULL);
 
