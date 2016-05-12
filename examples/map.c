a7f3b2333642db88ae87ea010c73340e5272e64a
Wilfried Meindl <wilfried.meindl@gmail.com>
2016-05-12 23:04:00 +0200

    Plug memory leaks in map example

--- examples/map.c
+++ examples/map.c
@@ -1,3 +1,4 @@
+#define _WITH_GETLINE
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
@@ -7,7 +8,7 @@
 #include "atom.h"
 
 #define NCHARS 4
-#define NELEMS 100000
+#define NELEMS 1000000
 
 void random_string(char *s, const int len) {
     static const char alphanum[] =
@@ -16,11 +17,11 @@ void random_string(char *s, const int len) {
 	"abcdefghijklmnopqrstuvwxyz";
     static const int alphanum_len = sizeof(alphanum) - 1;
 
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < len - 1; i++) {
 	s[i] = alphanum[rand() % alphanum_len];
     }
 
-    s[len] = 0;
+    s[len - 1] = 0;
 }
 
 int cmp(const char *key1, const char *key2, void *cl) {
@@ -28,11 +29,21 @@ int cmp(const char *key1, const char *key2, void *cl) {
     return strcmp(key1, key2);
 }
 
+char *copy_key(const char *key) {
+    char *new_key = CALLOC(NCHARS + 1, sizeof(char));
+    strcpy(new_key, key);
+    return new_key;
+}
+
 int *copy_data(const int *data) {
     int *new_data;
     NEW(new_data);
     *new_data = *data;
     return new_data;
+}
+
+void free_key(char *key) {
+    FREE(key);
 }
 
 void free_data(int *data) {
@@ -56,30 +67,34 @@ void wait_return(const char *message) {
 int main(void) {
     Map_T map = Map_new(
 	(Map_cmp_fun_T)cmp,
-	NULL,
-	(Map_copy_fun_T)copy_data,
-	NULL, (Map_free_fun_T)free_data);
+	(Map_copy_fun_T)copy_key, (Map_copy_fun_T)copy_data,
+	(Map_free_fun_T)free_key, (Map_free_fun_T)free_data);
 
     srand(time(NULL));
 
     wait_return("### Fill map");
-    printf("map->length = %d\n", Map_length(map));
-    char buf[NCHARS];
+    printf("map->length = %zu\n", Map_size(map));
+    char *buf;
     int n;
 
     for (int i = 0; i < NELEMS; i++) {
 	n = rand();
-	random_string(buf, NCHARS);
-	Map_insert(map, Atom_string(buf), copy_data(&n));
+	int *np = copy_data(&n);
+	buf = CALLOC(NCHARS + 1, sizeof(char));
+	random_string(buf, NCHARS+1);
+	if (!Map_insert(map, buf, np)) {
+	    FREE(buf);
+	    FREE(np);
+	}
     }
-    printf("map->length = %d\n", Map_length(map));
+    printf("map->length = %zu\n", Map_size(map));
 
     wait_return("### Traverse map");
     Map_traverse(map, pr, NULL);
 
     wait_return("### Copy map -> map2");
     Map_T map2 = Map_copy(map);
-    printf("map2->length = %d\n", Map_length(map));
+    printf("map2->length = %zu\n", Map_size(map));
 
     wait_return("### Free map");
     Map_free(&map);
